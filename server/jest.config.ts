b0886193f60882947a537b6678f86f60e6ce76d7
Conor Mullan <conor.mullan@unosquare.com>
2023-04-06 10:24:32 +0100

    Update jest.config.ts

--- server/jest.config.ts
+++ server/jest.config.ts
@@ -1,23 +1,23 @@
-// /*
-//  * For a detailed explanation regarding each configuration property and type check, visit:
-//  * https://jestjs.io/docs/configuration
-//  */
+/*
+ * For a detailed explanation regarding each configuration property and type check, visit:
+ * https://jestjs.io/docs/configuration
+ */
 
-// export default {
-//   clearMocks: true,
-//   collectCoverage: true,
-//   testPathIgnorePatterns: [
-//     "/dist/",
-//     "/node_modules/",
-//     "/prisma/",
-//     "/migrations/",
-//     "/coverage/*",
-//   ],
-//   coverageDirectory: "coverage",
-//   transform: {
-//     ".(ts)": "ts-jest",
-//   },
-//   testRegex: "(/__tests__/.*|\\.(test|spec))\\.(ts|tsx|js)$",
-//   moduleFileExtensions: ["ts", "js"],
-//   testTimeout: 10000,
-// };
+export default {
+  clearMocks: true,
+  collectCoverage: true,
+  testPathIgnorePatterns: [
+    "/dist/",
+    "/node_modules/",
+    "/prisma/",
+    "/migrations/",
+    "/coverage/*",
+  ],
+  coverageDirectory: "coverage",
+  transform: {
+    ".(ts)": "ts-jest",
+  },
+  testRegex: "(/__tests__/.*|\\.(test|spec))\\.(ts|tsx|js)$",
+  moduleFileExtensions: ["ts", "js"],
+  testTimeout: 10000,
+};
