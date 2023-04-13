42e926efddf0bbb1d06ba8306651e28502656dd3
Conor Mullan <conor.mullan@unosquare.com>
2023-04-13 11:06:19 +0100

    Update health.ts

--- server/controllers/health.ts
+++ server/controllers/health.ts
@@ -1,11 +1,14 @@
 import { Request, Response } from "express";
 import { StatusCodes } from "http-status-codes";
+import { prisma } from "../utils";
 
 async function getHealth(req: Request, res: Response) {
+  const allClients = await prisma.client.findMany();
   return res.status(StatusCodes.OK).json({
     name: process.env.npm_package_name,
     version: process.env.npm_package_version,
     status: "up",
+    clients: allClients,
   });
 }
 
